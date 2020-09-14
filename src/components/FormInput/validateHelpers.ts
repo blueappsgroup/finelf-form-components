3c950da97147150f00b019bb71281bbe716545fb
Dominika Czarniewska <dominika.czarniewska@gmail.com>
2020-09-14 14:52:15 +0200

    delete return, add type to funcs

--- src/components/FormInput/validateHelpers.ts
+++ src/components/FormInput/validateHelpers.ts
@@ -1,15 +1,14 @@
-export const isRequired = (value: string, required: boolean) => {
-    return (!value && required)
-}
+export const isRequired = (value: string, required: boolean): boolean =>
+  !value && required
 
-export const isBetweenNumberOfCharacters = (value: string, from = 2, to = 100) => {
-    return (value.length < from || value.length > to)
-}
+export const isBetweenNumberOfCharacters = (
+  value: string,
+  from = 2,
+  to = 100
+): boolean => value.length < from || value.length > to
 
-export const isValidPhoneNumber = (value: string) => {
-    return (!/^(45|50|51|53|57|60|66|69|72|73|78|79|88)[0-9]{7}$/.test(value) && value)
-}
+export const isValidPhoneNumber = (value: string): boolean =>
+  !/^(45|50|51|53|57|60|66|69|72|73|78|79|88)[0-9]{7}$/.test(value)
 
-export const isValidEmail = (value: string) => {
-    return (!/[a-zA-Z0-9_.+-]+@[a-zAZ0-9-]+\.[a-zA-Z0-9-.]+$/.test(value) && value)
-}
+export const isValidEmail = (value: string): boolean =>
+  !/[a-zA-Z0-9_.+-]+@[a-zAZ0-9-]+\.[a-zA-Z0-9-.]+$/.test(value)
