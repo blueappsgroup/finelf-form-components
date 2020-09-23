78b4f2ff9c0a7c6af76eea5e505a2a7278156aa4
Adrian <adrian.r@blueapps.pl>
2020-09-23 13:50:44 +0200

    refactoring handleOnChangeSlider in BaseRangeField

--- src/components/FormInput/base.tsx
+++ src/components/FormInput/base.tsx
@@ -274,9 +274,10 @@ export const BaseRangeField: (props: RangeFieldWrapProps) => ReactElement = ({
   const { id } = useContext(FormContext)
   const [value, setValue] = useState(parseInt(field.value) || props.value || 0)
   const handleOnChangeSlider = (value: number): void => {
-    values[field.name] = value
+    const newValues = values
+    newValues[field.name] = value
     setValue(value)
-    setFormValuesToCache(values, id)
+    setFormValuesToCache(newValues, id)
   }
   const handleOnChange = (e: ChangeEvent<HTMLInputElement>): void => {
     const value = parseInt(e.target.value) || 0
