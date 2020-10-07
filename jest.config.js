021c4cc3e1150081ecd520d66298f9d44033f68b
Mateusz Solecki <solmat@gmail.com>
2020-10-07 20:09:54 +0200

    update snapshots

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-
-  setupFilesAfterEnv: [
-    '<rootDir>/src/__test__/config/importJestDOM.ts',
-    '<rootDir>/src/__test__/config/globals.ts',
-  ],
-}
