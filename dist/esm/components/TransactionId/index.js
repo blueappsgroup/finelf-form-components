3fdb1416b1a0e272a1f16aa3423b39f91f05634b
tomaszbfinelf <tomasz.bogdziun@finelf.com>
2020-10-28 21:46:13 +0100

    dist update and ver to 1.9.5

--- dist/esm/components/TransactionId/index.js
+++ dist/esm/components/TransactionId/index.js
@@ -5,10 +5,10 @@ const StyledInput = styled.input`
   display: none;
 `;
 
-const TrasactionId = () => /*#__PURE__*/React.createElement(Field, {
-  name: "trasaction_id",
+const TransactionId = () => /*#__PURE__*/React.createElement(Field, {
+  name: "transaction_id",
   component: StyledInput
 });
 
-export default TrasactionId;
+export default TransactionId;
 //# sourceMappingURL=index.js.map
